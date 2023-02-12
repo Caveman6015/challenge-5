59dce27a6d790eebadc3a2c4d4fb6239da962c43
justin anderson <jcanderson@gmail.com>
2023-02-12 11:53:03 -0500

    fixed the local storage and time problems

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,39 @@
-var today = moment().format("dddd,MMM do YYYY");
+
+var today = moment().format("dddd, MMMM Do");
 var currentTime = moment().hour();
 $("#currentDay").html(today);
 
 $(".saveBtn").on("click", function () {
     let time = $(this).parent().attr("id");
-    let text = $(this).siblings(".description").val();
+    let text = $(this).siblings(".text-box").val();
+    console.log(text)
     localStorage.setItem(time, text);
   });
 
+  $(".time-block").each(function () {
+    let blockTime = ($(this).attr("id")); 
+  console.log(currentTime,blockTime)
+    if (blockTime == currentTime) {
+      $(this).addClass("present");
+      $(this).removeClass("future");
+      $(this).removeClass("past");
+    } else if (blockTime < currentTime) {
+      $(this).addClass("past");
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+    } else {
+      $(this).addClass("future");
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+    }
+  });
 
-
-$("#9am.description").val(localStorage.getItem("9am"));
-$("#10am.description").val(localStorage.getItem("10am"));
-$("#11am.description").val(localStorage.getItem("11am"));
-$("#12pm.description").val(localStorage.getItem("12pm"));
-$("#1pm.description").val(localStorage.getItem("1pm"));
-$("#2pm.description").val(localStorage.getItem("2pm"));
-$("#3pm.description").val(localStorage.getItem("3pm"));
-$("#4pm.description").val(localStorage.getItem("4pm"));
-$("#5pm.description").val(localStorage.getItem("5pm"));
+$("#9.description").val(localStorage.getItem("9am"));
+$("#10.description").val(localStorage.getItem("10am"));
+$("#11.description").val(localStorage.getItem("11am"));
+$("#12.description").val(localStorage.getItem("12pm"));
+$("#13.description").val(localStorage.getItem("1pm"));
+$("#14.description").val(localStorage.getItem("2pm"));
+$("#15.description").val(localStorage.getItem("3pm"));
+$("#16.description").val(localStorage.getItem("4pm"));
+$("#17.description").val(localStorage.getItem("5pm"));
